0473afb0989280c360c40b7fad735fe76a18241b
yixin <15070547025@163.com>
2022-03-22 20:03:16 +0800

    yi1.0

---
+++ java/ssm/spring-course/_01_spring/src/main/java/com/javayx/dao/ServiceDao.java
@@ -0,0 +1,12 @@
+package com.javayx.dao;
+
+/**
+ * @Author：易鑫
+ * @Version：1.0
+ * @Date：2022/3/22-14:16
+ * @Since:jdk1.8
+ * @Description:
+ */
+public interface ServiceDao {
+    void doSome();
+}
